3a2c55febdd47e666edfc8fe6caea95eee566564
oops-aman <b200054@nitsikkim.ac.in>
2022-04-04 23:34:11 +0530

    Project Euler #1: Multiples of 3 and 5

--- prob1.cpp
+++ prob1.cpp
@@ -7,37 +7,18 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    for (int m = 0; m < t; m++)
+    long N, num, three, five, fifteen = 0;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
     {
-        int n;
-        cin >> n;
-        int div_by_3[n], div_by_5[n], div[n];
-        int k = 0, j = 0, l = 0;
-        int sum1 = 0, sum2 = 0, sum3 = 0;
-        for (int i = 1; i < n; i++)
-        {
-            if (i % 15 == 0)
-            {
-                div[k] = i;
-                sum1 += i;
-                k++;
-            }
-            else if (i % 3 == 0)
-            {
-                div_by_3[j] = i;
-                sum2 += i;
-                j++;
-            }
-            else if (i % 5 == 0)
-            {
-                div_by_5[l] = i;
-                sum3 += i;
-                l++;
-            }
-        }
-        cout << (sum1 + sum2 + sum3) << endl;
+        cin >> num;
+        // int sum=0;
+        three = (num - 1) / 3;
+        five = (num - 1) / 5;
+        fifteen = (num - 1) / 15;
+
+        cout << 3 * (three * (three + 1) / 2) + 5 * (five * (five + 1) / 2) - 15 * (fifteen * (fifteen + 1) / 2) << endl;
     }
     return 0;
 }
